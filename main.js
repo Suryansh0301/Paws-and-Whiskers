519072c03d3ffa0b09dfcd967c60ea4b913a54cf
Suryansh <suryanshgoel31@gmail.com>
2024-10-09 05:40:48 +0530

    working on the pets area

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const template = document.querySelector("#pet-card-template")
+const wrapper = document.createDocumentFragment()
+
+
+
 async function start() {
   const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
   const weatherData = await weatherPromise.json()
@@ -11,11 +16,11 @@ async function petsArea() {
   const petsData = await petsPromise.json()
 
   petsData.forEach((element) => {
-
-    console.log(element.name);
-
-
+    const clone = template.content.cloneNode(true)
+    clone.querySelector("h3").textContent = element.name
+    wrapper.appendChild(clone)
   });
+  document.querySelector(".list-of-pets").appendChild(wrapper);
 
 
 }
